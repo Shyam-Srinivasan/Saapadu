45fe68f7cdf1347e431866bfd315ea3e85547c43
Shyam S <220701508@rajalakshmi.edu.in>
2025-08-29 17:52:01 +0530

    Made Admin-web using react

--- Saapadu/Admin - Web/frontend/src/App.jsx
+++ Saapadu/Admin - Web/frontend/src/App.jsx
@@ -7,6 +7,7 @@ import {HomePage} from "./components/HomePage";
 import {SignUpPage} from "./components/SignUpPage";
 import {SignInPage} from "./components/SignInPage";
 
+
 // import {SignUpPage} from './components/SignUpPage';
 // import {SignInPage} from "./components/SignInPage";
 // import {HomePage} from "./components/HomePage";
